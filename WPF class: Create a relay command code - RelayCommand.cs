f33bafa7db3b77eb99743983362baab13af25d88
Johnny <162920615+WhiskeyTangoCoding@users.noreply.github.com>
2024-05-15 11:27:53 -0600

    Create WPF class: Create a relay command code - RelayCommand.cs

---
+++ WPF class: Create a relay command code - RelayCommand.cs
@@ -0,0 +1,63 @@
+// // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+//--- XAML programming		(XAML code)		** XAML - Basics		RelayCommand.cs		By WhiskeyTango Coding
+
+** Class (WPF)
+
+*/
+
+/*====================================================    | GENERAL NOTES |    ======================================================*/
+
+** C. Create a relay command. 		(See Setup Notes below...)			** Folder: Core		Class
+** WPF .NET 4.8.1
+
+using System;
+using System.Windows.Input;
+
+namespace PayloadApp.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
+
+//---
+
+/*====================================================    | GENERAL NOTES |    ======================================================*/
+
+** IMPORTANT!!
+
+** We can use this for any command in any MVVM project w/o having to change this code at all
+
+
+
+
+
+
+/*====================================================    | GENERAL NOTES |    ======================================================*/
+
+//
